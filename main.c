8f65cc95d117d73db691fa882d88dc17badc7e0c
Emilio Lopez Berenguer <emilio@emiliollbb.net>
2023-06-19 08:28:28 +0200

    Fails

--- main.c
+++ main.c
@@ -18,19 +18,17 @@ int main(void);
 
 void load_word(void);
 void update_game(void);
+void init_game(void);
+void fail(void);
 
 const char word[12] =   {'R','I','N','O','C','E','R','O','N','T','E',0};
 char len = 11;
 char secret[12] = {'_','_','_','_','_','_','_','_','_','_','_',0};
 char current;
+char fail_count;
 
 int main() {
-    clear_screen();
-    
-    load_background(hang10);
-    clrscr();
-    
-    
+    init_game();
     
     printStr(10, 110, font, WHITE, BLACK, (char*)secret);
     
@@ -41,21 +39,68 @@ int main() {
     return 0;
 }
 
+void init_game() {
+    fail_count=0;
+    clear_screen();
+}
+
 void load_word() {
     
 }
 
+void fail() {
+    fail_count++;
+    if(fail_count==1) {
+        load_background(hang01);
+    }
+    if(fail_count==2) {
+        load_background(hang02);
+    }
+    if(fail_count==3) {
+        load_background(hang03);
+    }
+    if(fail_count==4) {
+        load_background(hang04);
+    }
+    if(fail_count==5) {
+        load_background(hang05);
+    }
+    if(fail_count==6) {
+        load_background(hang06);
+    }
+    if(fail_count==7) {
+        load_background(hang07);
+    }
+    if(fail_count==8) {
+        load_background(hang08);
+    }
+    if(fail_count==9) {
+        load_background(hang09);
+    }
+    if(fail_count==10) {
+        load_background(hang10);
+    }
+    
+    clrscr();
+}
+
 void update_game() {
-    char i, search, current;
+    char i, search, current, found;
     search = getchar();
     i=0;
+    found = 0;
     do {
         current=word[i];
         if(search == current) {
             secret[i]=word[i];
+            found = 1;
         }
         i++;
     } while(current != 0);
+    
+    if(found==0) {
+        fail();
+    }
         
     printStr(10, 110, font, WHITE, BLACK, (char*)secret);
 }
